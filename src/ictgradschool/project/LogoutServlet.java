6dbdfdf6ae74b02fddec1da7942f48bfbfab0d03
jluo669 <jluo669@aucklanduni.ac.nz>
2020-01-29 16:38:14 +1300

    logout

--- src/ictgradschool/project/LogoutServlet.java
+++ src/ictgradschool/project/LogoutServlet.java
@@ -6,15 +6,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+@WebServlet(name = "logout", urlPatterns = {"/logout"})
 public class LogoutServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String logout = "";
+
+        HttpSession session = req.getSession();
+        session.invalidate();
         RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.jsp");
         requestDispatcher.forward(req, resp);
     }
+
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        HttpSession session = req.getSession(false);
+//        if (session != null) {
+//            session.removeAttribute("userName");
+//        }
+//        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.jsp");
+//        requestDispatcher.forward(req, resp);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        doGet(req,resp);
+//    }
 }
