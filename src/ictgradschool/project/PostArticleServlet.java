a6c5f738223a4bf85297d003d3528a164abc53f5
sbro190 <d>
2020-01-29 16:07:41 +1300

    removed the testing session from PostArticleServlet.

--- src/ictgradschool/project/PostArticleServlet.java
+++ src/ictgradschool/project/PostArticleServlet.java
@@ -20,9 +20,6 @@ public class PostArticleServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        if (req.getSession(false).getAttribute("user") == null) TestSession.Create(this, req);
-
         int parentArticle;
         try {
             parentArticle = Integer.parseInt(req.getParameter("parentArticle"));
@@ -31,16 +28,14 @@ public class PostArticleServlet extends HttpServlet {
             parentArticle = -1;
         }
 
-        UserInfo user = (UserInfo)(req.getSession(false).getAttribute("user"));
-
-        Article article = new Article(((UserInfo)req.getSession(true).getAttribute("user")),
+        Article article = new Article(((UserInfo)req.getSession(false).getAttribute("user")),
                 req.getParameter("title"), req.getParameter("content"), new Timestamp((new Date()).getTime()),
                 new ArrayList<>(), parentArticle);
 
 
         try (Connection conn = DBConnectionUtils.getConnectionFromWebInf(this, "./res/connection.properties")) {
             ArticleDAO.insertArticle(conn, article);
-            
+
             req.setAttribute("articleID", article.getArticleId());
 
             req.getRequestDispatcher("/article").forward(req, resp);
