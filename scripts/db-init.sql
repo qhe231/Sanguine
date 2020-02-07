0df11c2c56c6b3440d8bb939040e69baa298103c
sbro190 <fr>
2020-02-07 16:20:30 +1300

    implemented like/dislike system for articles and comments

--- scripts/db-init.sql
+++ scripts/db-init.sql
@@ -1,5 +1,5 @@
 # @author: Peter He
-
+DROP TABLE IF EXISTS article_reactions;
 DROP TABLE IF EXISTS articles_and_comments;
 DROP TABLE IF EXISTS user_info;
 DROP TABLE IF EXISTS user_authentication;
@@ -38,4 +38,14 @@ CREATE TABLE IF NOT EXISTS articles_and_comments
   timeEdited     TIMESTAMP,
   FOREIGN KEY (parentId) REFERENCES articles_and_comments (id) ON DELETE CASCADE,
   FOREIGN KEY (userBelongedId) REFERENCES user_authentication (userId) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS article_reactions
+(
+  article_id INT NOT NULL,
+  user_id    INT NOT NULL,
+  reaction   INT NOT NULL,
+  PRIMARY KEY (user_id, article_id),
+  FOREIGN KEY (article_id) REFERENCES articles_and_comments (id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES user_authentication (userId) ON DELETE CASCADE
 );
---
+++ web/articleReaction.js
@@ -0,0 +1,69 @@
+window.addEventListener("load", function () {
+    const reactionButtons = document.querySelectorAll(".reactionButton");
+    for (let i = 0; i < reactionButtons.length; i++)
+        reactionButtons[i].addEventListener("click", react);
+    const reactionCounters = document.querySelectorAll(".reactionCounter");
+
+
+    const articleList = [];
+    const comments = document.querySelectorAll(".comment");
+    if (comments.length > 0) {
+        articleList.push(document.querySelector(".mainArticle").id);
+        for (let i = 0; i < comments.length; i++)
+            articleList.push(comments[i].id);
+    }
+    else {
+        const rows = document.querySelectorAll(".articleRow");
+        for (let i = 0; i < rows.length; i++)
+            articleList.push(rows[i].id);
+    }
+
+    checkCurrentReactions();
+
+    async function react() {
+        const articleId = this.id.substring(this.id.indexOf("-") + 1);
+        let reaction;
+        if (!this.checked)
+            reaction = 0;
+        else if (this.id.substring(0, this.id.indexOf("-")) == "like")
+            reaction = 1;
+        else
+            reaction = 2;
+
+        const promise = await fetch(`./react?articleId=${articleId}&reaction=${reaction}`, {
+            method: 'POST'
+        });
+        await checkCurrentReactions();
+    }
+
+    async function checkCurrentReactions() {
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'text/html');
+        const promise = await fetch("./articleReaction", {
+            method: 'POST',
+            headers: headers,
+            body: articleList
+        });
+
+        const currentReactions = await promise.json();
+
+        for (let i = 0; i < reactionCounters.length; i++) {
+            const j = Math.floor(i / 2);
+
+            if (reactionButtons.length > 0) {
+                if (currentReactions[j].user == -1)
+                    reactionButtons[i].disabled = true;
+                else if (currentReactions[j].user > 0 && ((currentReactions[j].user - 1) % 2 == i % 2))
+                    reactionButtons[i].checked = true;
+                else
+                    reactionButtons[i].checked = false;
+            }
+
+            if (i % 2 == 0)
+                reactionCounters[i].innerText = currentReactions[j].likes;
+            else
+                reactionCounters[i].innerText = currentReactions[j].dislikes;
+        }
+    }
+});
