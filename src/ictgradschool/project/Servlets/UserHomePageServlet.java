3971917c621b446c90c83590b65be26e5d129720
sbro190 <s>
2020-02-11 13:49:34 +1300

    fixed user homepage bug

--- src/ictgradschool/project/Servlets/UserHomePageServlet.java
+++ src/ictgradschool/project/Servlets/UserHomePageServlet.java
@@ -31,39 +31,18 @@ public class UserHomePageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Object userObj = req.getSession().getAttribute("user");
         UserInfo user = (userObj == null) ? null : (UserInfo) userObj;
-//        List<Article> comments = new ArrayList<>();
 
         try (Connection conn = DBConnectionUtils.getConnectionFromClasspath("connection.properties")) {
             UserInfo owner = UserInfoDAO.getUserInfoByUserName(conn, req.getParameter("owner"));
             List<Article> articles = ArticleDAO.getArticles(conn, -1, owner.getUserId(), true);
-            List<Article> comments = ArticleDAO.getAllCommentsForSpecificUser(conn,user.getUserId());
-
-//            for (Article a : articles) {
-//
-//                //add each article's comment in the comments list
-//                for (Article c : a.getChildren()) {
-//                    comments.add(c);
-//                }
-//            }
-
-//            List<Article> allArticles = ArticleDAO.getArticles(conn, -1, -1, true);
-//            for (Article article : allArticles) {
-//                List<Article> children = article.getChildren();
-//                for (Article child : children) {
-//                    if (child.getAuthor().getUserId() == user.getUserId()) {
-//                        comments.add(child);
-//                    }
-//                    while (child.getChildren() != null) {
-//                        children = child.getChildren();
-//                    }
-//                }
-//
-//            }
 
             req.setAttribute("articles", articles);
-            req.setAttribute("user", user);
             req.setAttribute("owner", owner);
-            req.setAttribute("comments", comments);
+            req.setAttribute("user", user);
+            if (user != null) {
+                List<Article> comments = ArticleDAO.getAllCommentsForSpecificUser(conn, user.getUserId());
+                req.setAttribute("comments", comments);
+            }
 
             req.getRequestDispatcher("/userHomePage.jsp").forward(req, resp);
 
